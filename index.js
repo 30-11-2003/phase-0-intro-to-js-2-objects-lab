6627b93a1df41e5fb6d48e87bfdd1b0e25d313b7
ShalomOrangi <145541724+30-11-2003@users.noreply.github.com>
2023-10-09 08:54:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+const employee={
+  name:"Jimmy",
+  streetAddress:"Milimani"
+ 
+ }
+ 
+  function updateEmployeeWithKeyAndValue(){
+    newEmployeeName:"Sam"
+    
+  }
+  
+ function destructivelyUpdateEmployeeWithKeyAndValue(){
+   employee ['name']="Sam"
+   employee ['streetAddress']="11 Broadway"
+}
+ 
+  function deleteFromEmployeeByKey(){
+    delete newEmployeeName="Sam"
+  }
+  
